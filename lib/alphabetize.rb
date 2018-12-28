ca031a2dc1cae2af13c265b7d0b0229b072bef2d
Victoria B <viktorija.bourassa@gmail.com>
2018-12-28 18:31:12 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 
 def alphabetize(arr)
-  esperanto_alphabet = ESPERANTO_ALPHABET.split(", ")
+  esperanto_alphabet = ESPERANTO_ALPHABET.split(",")
   arr.sort_by do |word|
     word.split('').collect do |letter|
       esperanto_alphabet.index(letter)
