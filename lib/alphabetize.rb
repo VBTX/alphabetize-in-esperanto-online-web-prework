281b9c153266be2c7d7db0a23be24cd27d60f25f
Victoria B <viktorija.bourassa@gmail.com>
2018-12-28 18:19:47 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 
 def alphabetize(arr)
   arr.sort_by do |sentence|
-    sentence.split("").map do |character|
+    sentence.split(" ").map do |character|
       ESPERANTO_ALPHABET.index(character)
     end
   end
